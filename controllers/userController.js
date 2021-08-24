a61d46ab27564c6e955eff6a72424506fcf6acbe
VladPetriv <vladpetriv05@gmail.com>
2021-08-24 13:18:46 +0300

    finished work with user controller,router

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { generateToken } = require('../utils/jwtUtils.js');
 const UserService = require('../services/userService.js');
 const CartService = require('../services/cartService.js');
-const { encrypt, decrypt } = require('../utils/passwordUtils.js');
 
 class UserController {
   async registarion(req, res) {
@@ -9,26 +9,38 @@ class UserController {
       const { login, password } = req.body;
       const candidate = await UserService.getOne(login);
       if (candidate) {
-        return res.json('User with this login is exist');
+        return res.json('Please use another login');
       }
-      const hashPassword = encrypt(password);
+      const hashPassword = await bcrypt.hash(password, 5);
       const user = await UserService.create(login, hashPassword);
       const cart = await CartService.create(user.id);
-      const token = jwt.sign(
-        { id: user.id, login, cart },
-        process.env.SECRET_KEY,
-        {
-          expiresIn: '24h',
-        }
-      );
-      return res.json(token);
+      const token = generateToken(user.id, login);
+      return res.json({ token });
     } catch (err) {
-      console.log(err);
       res.status(500).json(err);
     }
   }
-  async login(req, res) {}
-  async checkStatus(req, res) {}
+  async login(req, res) {
+    try {
+      const { login, password } = req.body;
+      const user = await UserService.getOne(login);
+      if (!user) {
+        return res.status(400).json('Incorrect login');
+      }
+      const comparePassword = bcrypt.compareSync(password, user.password);
+      if (!comparePassword) {
+        return res.status(400).json('Incorrect password');
+      }
+      const token = generateToken(user.id, login);
+      return res.json({ token });
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  }
+  async check() {
+    const token = generateJwt(req.user.id, req.user.email);
+    return res.json({ token });
+  }
 }
 
 module.exports = new UserController();
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,11 @@
 const Router = require('express');
+const authmiddlewares = require('../middlewares/chechUserMiddleware');
 const UserController = require('../controllers/userController.js');
 
 const router = Router();
 
 router.post('/registration', UserController.registarion);
 router.post('/login', UserController.login);
-router.get('/auth', UserController.checkStatus);
+router.get('/auth', authmiddlewares, UserController.check);
 
 module.exports = router;
