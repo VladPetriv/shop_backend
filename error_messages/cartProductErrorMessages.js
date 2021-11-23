d3b05b11e20059990b8365a043981f2a435bb82d
VladPetriv <vladpetriv05@gmail.com>
2021-11-23 20:11:50 +0000

    Update user and create cart product error messages

---
+++ error_messages/cartProductErrorMessages.js
@@ -0,0 +1,3 @@
+const NO_CART_PRODUCT_WITH_ID = 'Cart product with this id doesnt exist';
+
+export { NO_CART_PRODUCT_WITH_ID };
---
+++ error_messages/userErrorMessages.js
@@ -0,0 +1,17 @@
+const USE_ANOTHER_LOGIN = 'User with this login is exist';
+const INCORRECT_LOGIN = 'Incorrect login';
+const INCORRECT_PASSWORD = 'Incorrect password';
+const NOT_VALID_EMAIL = 'Please use valid email';
+const NOT_VALID_LOGIN = 'MUst be 4+ and less then 10 symbols';
+const NOT_VALID_PASSWORD = 'Must be 4+ symbols';
+const USER_NOT_EXIST = 'User with this login doesnt exist';
+
+export {
+  USE_ANOTHER_LOGIN,
+  INCORRECT_PASSWORD,
+  INCORRECT_LOGIN,
+  NOT_VALID_LOGIN,
+  NOT_VALID_EMAIL,
+  NOT_VALID_PASSWORD,
+  USER_NOT_EXIST,
+};
