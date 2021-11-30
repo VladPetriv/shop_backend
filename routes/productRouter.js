05bfa00a6784686a7e7cbcaac2d4ee2cb7a10afe
VladPetriv <vladpetriv05@gmail.com>
2021-11-30 10:50:28 +0000

    add check role middleware for product route and test it

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { body } from 'express-validator';
 import ProductController from '../controllers/productController.js';
 import { NOT_VALID_NAME } from '../error_messages/productErrorMessages.js';
+import checkUserRole from '../middlewares/checkUserRoleMiddleware.js';
 
 const router = Router();
 
@@ -9,10 +10,19 @@ router.get('/items/', ProductController.getAllProduct);
 router.get('/items/:id', ProductController.getOneProduct);
 router.post(
   '/create',
+  checkUserRole('ADMIN'),
   body('name').isLength({ min: 2, max: 15 }).withMessage(NOT_VALID_NAME),
   ProductController.createProduct
 );
-router.delete('/items/:id', ProductController.deleteProduct);
-router.put('/items/:id', ProductController.updateProduct);
+router.delete(
+  '/items/:id',
+  checkUserRole('ADMIN'),
+  ProductController.deleteProduct
+);
+router.put(
+  '/items/:id',
+  checkUserRole('ADMIN'),
+  ProductController.updateProduct
+);
 
 export default router;
--- test/product.test.js
+++ test/product.test.js
@@ -6,6 +6,7 @@ import {
   productTestHelper,
   brandTestHelper,
   typeTestHelper,
+  userTestHelper,
 } from './testHelper.js';
 import {
   NO_PRODUCT_WITH_ID,
@@ -21,6 +22,7 @@ describe('Product test', () => {
   describe('GET products => /api/product/items', () => {
     it('It should get products', async () => {
       const response = await request.get('/api/product/items');
+
       expect(response.status).toBe(200);
       expect(response.body.length).toBe(0);
       expect(response.body).toStrictEqual([]);
@@ -32,8 +34,10 @@ describe('Product test', () => {
     beforeEach(async () => {
       id = await productTestHelper().addNewTestProduct();
     });
+
     it('It should return product', async () => {
       const response = await request.get(`/api/product/items/${id}`);
+
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('id');
       expect(response.body).toHaveProperty('name');
@@ -45,8 +49,10 @@ describe('Product test', () => {
       expect(response.body.name).toBe('test');
       expect(response.body.price).toBe(1000);
     });
+
     it('it should throw error that product with this id doesnt exist', async () => {
       const response = await request.get(`/api/product/items/${id + 1}`);
+
       expect(response.status).toBe(400);
       expect(response.body).toHaveProperty('message');
       expect(response.body.message).toBe(NO_PRODUCT_WITH_ID);
@@ -54,36 +60,100 @@ describe('Product test', () => {
   });
 
   describe('DELETE product => /api/product/items/id', () => {
-    let id;
+    let productId;
+    let jwt;
+    let jwt2;
+    const login = 'test';
+    const email = 'testfajsfde@test.com';
+    const password = 'testfjasdfjk';
+    const role = 'ADMIN';
+
     beforeEach(async () => {
-      id = await productTestHelper().addNewTestProduct();
+      productId = await productTestHelper().addNewTestProduct();
+      await userTestHelper().createTestUser(login, email, password, role);
+      await userTestHelper().createTestUser(
+        'testlogin',
+        'testemeil@test.com',
+        'testpassword'
+      );
+      jwt = await request.post('/api/login').send({ login, email, password });
+      jwt2 = await request.post('/api/login').send({
+        login: 'testlogin',
+        email: 'testemail@test.com',
+        password: 'testpassword',
+      });
     });
+
     it('it should delete product', async () => {
-      const response = await request.delete(`/api/product/items/${id}`);
+      const response = await request
+        .delete(`/api/product/items/${productId}`)
+        .set('Authorization', 'Bearer ' + jwt.body.token);
 
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('message');
       expect(response.body.message).toBe('Product was deleted');
     });
+
     it('it should throw error that product with this id doesnt exist', async () => {
-      const response = await request.delete(`/api/product/items/${id + 1}`);
+      const response = await request
+        .delete(`/api/product/items/${productId + 1}`)
+        .set('Authorization', 'Bearer ' + jwt.body.token);
+
       expect(response.status).toBe(400);
       expect(response.body).toHaveProperty('message');
       expect(response.body.message).toBe(NO_PRODUCT_WITH_ID);
+    });
+
+    it('it should throw error that user is not logined', async () => {
+      const response = await request.delete(`/api/product/items/${productId}`);
+
+      expect(response.status).toBe(401);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toBe('User is not logined');
+    });
+
+    it('it should throw error that user dont have accsess', async () => {
+      const response = await request
+        .delete(`/api/product/items/${productId}`)
+        .set('Authorization', 'Bearer ' + jwt2.body.token);
+
+      expect(response.status).toBe(403);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toBe('No accsess');
     });
   });
 
   describe('POST product => /api/product/', () => {
     let typeId;
     let brandId;
+    let jwt;
+    let jwt2;
+    const login = 'test';
+    const email = 'testfajsfde@test.com';
+    const password = 'testfjasdfjk';
+    const role = 'ADMIN';
+
     beforeEach(async () => {
       typeId = await typeTestHelper().addNewTestType();
       brandId = await brandTestHelper().addNewTestBrand();
+      await userTestHelper().createTestUser(login, email, password, role);
+      await userTestHelper().createTestUser(
+        'testlogin',
+        'testemeil@test.com',
+        'testpassword'
+      );
+      jwt = await request.post('/api/login').send({ login, email, password });
+      jwt2 = await request.post('/api/login').send({
+        login: 'testlogin',
+        email: 'testemail@test.com',
+        password: 'testpassword',
+      });
     });
 
     it('it should create new product', async () => {
       const response = await request
         .post('/api/product/create')
+        .set('Authorization', 'Bearer ' + jwt.body.token)
         .field('name', 'test_product')
         .field('price', 1000)
         .field('description', 'test description')
@@ -108,9 +178,11 @@ describe('Product test', () => {
       expect(response.body.product.brandId).toBe(brandId);
       expect(response.body.message).toBe('Product was created');
     });
+
     it('it should throw error that name is not valid', async () => {
       const response = await request
         .post('/api/product/create')
+        .set('Authorization', 'Bearer ' + jwt.body.token)
         .field('name', 't')
         .field('price', 1000)
         .field('description', 'test description')
@@ -121,15 +193,68 @@ describe('Product test', () => {
       expect(response.body).toHaveProperty('errors');
       expect(response.body.errors[0].msg).toBe(NOT_VALID_NAME);
     });
+
+    it('it should throw error that user is not logined', async () => {
+      const response = await request
+        .post('/api/product/create')
+        .field('name', 't')
+        .field('price', 1000)
+        .field('description', 'test description')
+        .field('typeId', typeId)
+        .field('brandId', brandId)
+        .attach('img', join(resolve(), '/' + process.env.IMAGE_NAME));
+
+      expect(response.status).toBe(401);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toBe('User is not logined');
+    });
+
+    it('it should throw error that user dont have accsess', async () => {
+      const response = await request
+        .post('/api/type/create')
+        .set('Authorization', 'Bearer ' + jwt2.body.token)
+        .field('name', 't')
+        .field('price', 1000)
+        .field('description', 'test description')
+        .field('typeId', typeId)
+        .field('brandId', brandId)
+        .attach('img', join(resolve(), '/' + process.env.IMAGE_NAME));
+
+      expect(response.status).toBe(403);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toBe('No accsess');
+    });
   });
+
   describe('PUT product => /api/product/items/:productId', () => {
     let productId;
+    let jwt;
+    let jwt2;
+    const login = 'test';
+    const email = 'testfajsfde@test.com';
+    const password = 'testfjasdfjk';
+    const role = 'ADMIN';
+
     beforeEach(async () => {
       productId = await productTestHelper().addNewTestProduct();
+      await userTestHelper().createTestUser(login, email, password, role);
+      await userTestHelper().createTestUser(
+        'testlogin',
+        'testemeil@test.com',
+        'testpassword'
+      );
+      jwt = await request.post('/api/login').send({ login, email, password });
+      jwt2 = await request.post('/api/login').send({
+        login: 'testlogin',
+        email: 'testemail@test.com',
+        password: 'testpassword',
+      });
     });
+
     it('it should update product and return message', async () => {
       const response = await request
         .put(`/api/product/items/${productId}`)
+        .set('Authorization', 'Bearer ' + jwt.body.token)
         .send({
           name: 'test1',
           price: 1500,
@@ -139,12 +264,33 @@ describe('Product test', () => {
       expect(response.body).toHaveProperty('message');
       expect(response.body.message).toBe('Product was updated');
     });
+
     it('it should throw error that product is not exist', async () => {
-      const response = await request.put(`/api/product/items/${productId + 1}`);
+      const response = await request
+        .put(`/api/product/items/${productId + 1}`)
+        .set('Authorization', 'Bearer ' + jwt.body.token);
 
       expect(response.status).toBe(400);
       expect(response.body).toHaveProperty('message');
       expect(response.body.message).toBe(NO_PRODUCT_WITH_ID);
+    });
+
+    it('it should throw error that user is not logined', async () => {
+      const response = await request.put(`/api/product/items/${productId}`);
+
+      expect(response.status).toBe(401);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toBe('User is not logined');
+    });
+
+    it('it should throw error that user dont have accsess', async () => {
+      const response = await request
+        .put(`/api/product/items/${productId}`)
+        .set('Authorization', 'Bearer ' + jwt2.body.token);
+
+      expect(response.status).toBe(403);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toBe('No accsess');
     });
   });
 });
