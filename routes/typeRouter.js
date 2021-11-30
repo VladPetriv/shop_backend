63d1ce3ea756609f8df98c0cb924e684ec2ac415
VladPetriv <vladpetriv05@gmail.com>
2021-11-30 10:17:07 +0000

    add check role middleware for type route and test it

--- routes/typeRouter.js
+++ routes/typeRouter.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { body } from 'express-validator';
 import TypeController from '../controllers/typeController.js';
 import { NOT_VALID_NAME } from '../error_messages/typeErrorMessages.js';
+import checkUserRole from '../middlewares/checkUserRoleMiddleware.js';
 
 const router = Router();
 
@@ -9,10 +10,11 @@ router.get('/items/', TypeController.getAllTypes);
 router.get('/items/:id', TypeController.getOneType);
 router.post(
   '/create',
+  checkUserRole('ADMIN'),
   body('name').isLength({ min: 2, max: 15 }).withMessage(NOT_VALID_NAME),
   TypeController.createType
 );
-router.delete('/items/:id', TypeController.deleteType);
-router.put('/items/:id', TypeController.updateType);
+router.delete('/items/:id', checkUserRole('ADMIN'), TypeController.deleteType);
+router.put('/items/:id', checkUserRole('ADMIN'), TypeController.updateType);
 
 export default router;
--- test/type.test.js
+++ test/type.test.js
@@ -1,6 +1,6 @@
 import supertest from 'supertest';
 import app from '../index.js';
-import { typeTestHelper } from './testHelper.js';
+import { typeTestHelper, userTestHelper } from './testHelper.js';
 import {
   NOT_VALID_NAME,
   NO_TYPE_WITH_ID,
@@ -9,29 +9,41 @@ import {
 const request = supertest(app);
 
 describe('Type test', () => {
-  beforeEach(async () => await typeTestHelper().destroyAllTypes());
-  afterAll(async () => await typeTestHelper().destroyAllTypes());
+  beforeEach(async () => {
+    await typeTestHelper().destroyAllTypes();
+    await userTestHelper().destroyTestUser();
+  });
+  afterAll(async () => {
+    await typeTestHelper().destroyAllTypes();
+    await userTestHelper().destroyTestUser();
+  });
+
   describe('GET types => /api/type/items', () => {
     it('it should return all types and 200 as status code', async () => {
       const response = await request.get('/api/type/items');
+
       expect(response.status).toBe(200);
       expect(response.body).toStrictEqual([]);
       expect(response.body.length).toBe(0);
     });
   });
+
   describe('GET type => /api/type/items/id', () => {
     let id;
     beforeEach(async () => {
       id = await typeTestHelper().addNewTestType();
     });
+
     it('it should return type and 200 as status code', async () => {
       const response = await request.get(`/api/type/items/${id}`);
+
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('id');
       expect(response.body).toHaveProperty('name');
       expect(response.body.id).toBe(id);
       expect(response.body.name).toBe('test');
     });
+
     it('it should throw error that type with this id doesnt exits', async () => {
       const response = await request.get(`/api/type/items/${id + 1}`);
       expect(response.status).toBe(400);
@@ -39,21 +51,74 @@ describe('Type test', () => {
       expect(response.body.message).toBe(NO_TYPE_WITH_ID);
     });
   });
+
   describe('POST type => /api/type/create', () => {
-    it('It should create type and return 200 as status code', async () => {
-      const response = await request.post('/api/type/create').send({
-        name: 'test',
+    let jwt;
+    let jwt2;
+    const login = 'test';
+    const email = 'testfajsfde@test.com';
+    const password = 'testfjasdfjk';
+    const role = 'ADMIN';
+
+    beforeEach(async () => {
+      await userTestHelper().createTestUser(login, email, password, role);
+      await userTestHelper().createTestUser(
+        'testlogin',
+        'testemeil@test.com',
+        'testpassword'
+      );
+      jwt = await request.post('/api/login').send({ login, email, password });
+      jwt2 = await request.post('/api/login').send({
+        login: 'testlogin',
+        email: 'testemail@test.com',
+        password: 'testpassword',
       });
+    });
+
+    it('It should create type', async () => {
+      const response = await request
+        .post('/api/type/create')
+        .set('Authorization', 'Bearer ' + jwt.body.token)
+        .send({
+          name: 'test',
+        });
+
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('type');
       expect(response.body).toHaveProperty('message');
       expect(response.body.type.name).toBe('test');
       expect(response.body.message).toBe('Type was created');
     });
-    it('it should throw error that name is not valid', async () => {
+
+    it('it should throw error that user is not logined', async () => {
       const response = await request.post('/api/type/create').send({
-        name: 't',
+        name: 'test',
       });
+
+      expect(response.status).toBe(401);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toBe('User is not logined');
+    });
+
+    it('it should throw error that user dont have accsess', async () => {
+      const response = await request
+        .post('/api/type/create')
+        .set('Authorization', 'Bearer ' + jwt2.body.token)
+        .send({ name: 'test' });
+
+      expect(response.status).toBe(403);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toBe('No accsess');
+    });
+
+    it('it should throw error that name is not valid', async () => {
+      const response = await request
+        .post('/api/type/create')
+        .set('Authorization', 'Bearer ' + jwt.body.token)
+        .send({
+          name: 't',
+        });
+
       expect(response.status).toBe(400);
       expect(response.body).toHaveProperty('errors');
       expect(response.body.errors[0].msg).toBe(NOT_VALID_NAME);
@@ -62,39 +127,127 @@ describe('Type test', () => {
 
   describe('DELETE type => /api/type/items/id', () => {
     let id;
+    let jwt;
+    let jwt2;
+    const login = 'test';
+    const email = 'testfajsfde@test.com';
+    const password = 'testfjasdfjk';
+    const role = 'ADMIN';
+
     beforeEach(async () => {
       id = await typeTestHelper().addNewTestType();
+      await userTestHelper().createTestUser(login, email, password, role);
+      await userTestHelper().createTestUser(
+        'testlogin',
+        'testemeil@test.com',
+        'testpassword'
+      );
+      jwt = await request.post('/api/login').send({ login, email, password });
+      jwt2 = await request.post('/api/login').send({
+        login: 'testlogin',
+        email: 'testemail@test.com',
+        password: 'testpassword',
+      });
     });
+
     it('It should delete type and return message', async () => {
-      const response = await request.delete(`/api/type/items/${id}`);
+      const response = await request
+        .delete(`/api/type/items/${id}`)
+        .set('Authorization', 'Bearer ' + jwt.body.token);
 
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('message');
       expect(response.body.message).toBe('Type was deleted');
     });
+
     it('it should throw error that type doesnt exist', async () => {
-      const response = await request.delete(`/api/type/items/${id + 1}`);
+      const response = await request
+        .delete(`/api/type/items/${id + 1}`)
+        .set('Authorization', 'Bearer ' + jwt.body.token);
+
       expect(response.status).toBe(400);
       expect(response.body).toHaveProperty('message');
       expect(response.body.message).toBe(NO_TYPE_WITH_ID);
+    });
+
+    it('it should throw error that user is not logined', async () => {
+      const response = await request.delete(`/api/type/items/${id}`);
+
+      expect(response.status).toBe(401);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toBe('User is not logined');
+    });
+
+    it('it should throw error that user dont have accsess', async () => {
+      const response = await request
+        .delete(`/api/type/items/${id}`)
+        .set('Authorization', 'Bearer ' + jwt2.body.token);
+
+      expect(response.status).toBe(403);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toBe('No accsess');
     });
   });
 
   describe('PUT type => /api/type/items/id', () => {
     let id;
+    let jwt;
+    let jwt2;
+    const login = 'test';
+    const email = 'testfajsfde@test.com';
+    const password = 'testfjasdfjk';
+    const role = 'ADMIN';
+
     beforeEach(async () => {
       id = await typeTestHelper().addNewTestType();
+      await userTestHelper().createTestUser(login, email, password, role);
+      await userTestHelper().createTestUser(
+        'testlogin',
+        'testemeil@test.com',
+        'testpassword'
+      );
+      jwt = await request.post('/api/login').send({ login, email, password });
+      jwt2 = await request.post('/api/login').send({
+        login: 'testlogin',
+        email: 'testemail@test.com',
+        password: 'testpassword',
+      });
     });
+
     it('it should update type', async () => {
       const response = await request
         .put(`/api/type/items/${id}`)
+        .set('Authorization', 'Bearer ' + jwt.body.token)
         .send({ name: 'updated_type' });
+
       expect(response.status).toBe(200);
       expect(response.body).toHaveProperty('message');
       expect(response.body.message).toBe('Type was updated');
     });
+
+    it('it should throw error that user is not logined', async () => {
+      const response = await request.put(`/api/type/items/${id}`);
+
+      expect(response.status).toBe(401);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toBe('User is not logined');
+    });
+
+    it('it should throw error that user dont have accsess', async () => {
+      const response = await request
+        .put(`/api/type/items/${id}`)
+        .set('Authorization', 'Bearer ' + jwt2.body.token);
+
+      expect(response.status).toBe(403);
+      expect(response.body).toHaveProperty('message');
+      expect(response.body.message).toBe('No accsess');
+    });
+
     it('it should throw error that type doesnt exist', async () => {
-      const response = await request.put(`/api/type/items/${id + 1}`);
+      const response = await request
+        .put(`/api/type/items/${id + 1}`)
+        .set('Authorization', 'Bearer ' + jwt.body.token);
+
       expect(response.status).toBe(400);
       expect(response.body).toHaveProperty('message');
       expect(response.body.message).toBe(NO_TYPE_WITH_ID);
